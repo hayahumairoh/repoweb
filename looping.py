06ee7df0951eb33132fd698a9337261a7415b43e
hayahumairoh <humairohhaya0922@gmail.com>
2023-03-10 14:36:11 +0700

    update vol.1

--- looping.py
+++ looping.py
@@ -1,3 +1,11 @@
+#source code nomor 1
+
+print('Numbers from 1 to 100:')
+for n in range(1, 101):
+    print(n, end=' ')
+
+#source code nomor 2
+
 for i in range(1, 101):
     if i % 2 == 0:
         continue
